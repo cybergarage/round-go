6e9609a70edec4b5c4c038e4d48879272166e445
Satoshi Konno <skonno@cybergarage.org>
2015-06-22 08:31:18 +0900

    * Updated JavaScript functions.

--- src/round/script/java_script_engine_test.go
+++ src/round/script/java_script_engine_test.go
@@ -2,12 +2,20 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package core
+package script
 
 import (
-	"errors"
+	"round/core"
 	"testing"
 )
 
 func TestNewJavaScriptEngine(t *testing.T) {
+	jsEngine := NewJavaScriptEngine()
+
+	script := core.NewScript()
+	script.Code = []byte("var result = 1 + 2 + 3;\n result;")
+	_, err := jsEngine.Run(script, "")
+	if err != nil {
+		t.Error(err)
+	}
 }
