aac220de2fbcae0f5563a15dfd5fefdf94b80750
Satoshi Konno <skonno@cybergarage.org>
2015-06-25 00:58:57 +0900

    * Added MessageQueue.

--- src/round/core/message_test.go
+++ src/round/core/message_test.go
@@ -2,20 +2,12 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package rpc
+package core
 
 import (
-	"errors"
 	"testing"
 )
 
-const (
-	nullMessageError = "Message is null"
-)
-
 func TestNewMessage(t *testing.T) {
-	msg := NewMessage()
-	if msg == nil {
-		t.Error(errors.New(nullMessageError))
-	}
+	//msg := NewMessage("")
 }
